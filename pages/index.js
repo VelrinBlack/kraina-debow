c2cdfb9e55586e1b612579f619d2cb35a42bc1d5
Velrin Black <velrinblack@gmail.com>
2021-03-13 17:13:57 +0100

    Add home page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Page from 'components/templates/Page/Page';
+import About from 'components/organisms/About/About';
+
+const Home = () => (
+  <Page>
+    <About />
+  </Page>
+);
+
+export default Home;
