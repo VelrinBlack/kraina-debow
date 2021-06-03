6b7e203278876e6a4cae6381b361268aa63686d1
Velrin Black <velrinblack@gmail.com>
2021-06-03 11:32:56 +0200

    Make content changes

--- components/organisms/About/About.js
+++ components/organisms/About/About.js
@@ -6,18 +6,17 @@ const About = () => (
     <div className="text-container">
       <h2>Poznajmy się!</h2>
       <p>
-        Mam na imię Kasia. Dwa lata temu zamieszkaliśmy z mężem, dziećmi i
-        naszymi ukochanymi czworonogami pod puszczą, z dala od zgiełku miasta w
-        wymarzonym domu z ogrodem. Wtedy to rozpoczęliśmy magiczną podróż do
-        zaczarowanego świata natury, zwierząt leśnych, prastarych drzew -
-        pełnego spokoju i harmonii. Strumień niezwykłych doświadczeń, jakie
-        płyną z obcowania z naturą, wskazał mi zupełnie nowy wymiar
-        rzeczywistości. Dwudziestoletnie doświadczenie pracy z drugim
-        człowiekiem, zamiłowanie do psychologii, naturoterapii, ajurwedy, a w
-        końcu ukończenie studiów tychże obszarów domyka we mnie przekonanie, że
-        człowiek nie jest częścią natury. On jest naturą! Poszukuje szczęścia,
-        lecz często nie tam, gdzie ono jest - w świecie zewnętrznym, zdala od
-        źródła.
+        Mam na imię Kasia. Dwa lata temu zamieszkałam z rodziną pod puszczą, z
+        dala od zgiełku miasta w wymarzonym domu z ogrodem. Wtedy to
+        rozpoczęliśmy magiczną podróż do zaczarowanego świata natury, zwierząt
+        leśnych, prastarych drzew - pełnego spokoju i harmonii. Strumień
+        niezwykłych doświadczeń, jakie płyną z obcowania z naturą, wskazał mi
+        zupełnie nowy wymiar rzeczywistości. Dwudziestoletnie doświadczenie
+        pracy z drugim człowiekiem, zamiłowanie do psychologii, naturoterapii,
+        ajurwedy, a w końcu ukończenie studiów w tychże obszarach domyka we mnie
+        przekonanie, że człowiek nie jest częścią natury. On jest naturą!
+        Poszukuje szczęścia, lecz często nie tam, gdzie ono jest - w świecie
+        zewnętrznym, zdala od źródła.
         <br />
         <br />
         Zapraszam Cię do niezwykłej podróży, w głąb natury. Kiedy już tam
